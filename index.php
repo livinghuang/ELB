616feee4ce08c589d7342bc4d90cedc19841747d
livinghuang <living.huang@gmail.com>
2017-01-09 17:59:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php
-@data_default_timezone_set("Asia/Taipei");
-$current_datetime = @date('Y-m-d H:i:s',@time());
-echo "service @current_datetime:".file_get_contents('/etc/hostname')."\n";
+<?php 
+date_default_timezone_set("Asia/Taipei");  //設定時區 
+echo date("Y年m月j日 G時i分s秒");  //第1個參數是時間格式；第2個參數是引數，沒有引數時，默認為現在時間。 
+//date("Y-n-j G:i:s",1393224543);  //逗點之後的數值為引數。 
+?>
