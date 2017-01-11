556cbf0895616fef8729c8ce2ea83fac5967cdfd
living.huang <living.huang@gmail.com>
2017-01-11 14:47:31 +0800

    4th commit

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
+<html>
+<html>
+<head>
+<title>Encode Software</title>
+<link rel="stylesheet" href="encryptor.css">
+</head>
+<body>
+<div class="menu">
+<a href="<?php echo $_SERVER['PHP_SELF']; ?>">Home</a> -
+<a href="<?php echo $_SERVER['PHP_SELF'].'?op=record'; ?>">Record</a> -
+<a href="<?php echo $_SERVER['PHP_SELF'].'?op=about'; ?>"About</a>
+</div>
+<div class="main">
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+<input type="text" name="input" size="67"><br / >
+<input type="submit" value="New" name="op">
+<input type="submit" value="Load" name="op">
+<input type="submit" value="Encode" name="op">
+<input type="submit" value="Decode" name="op">
+<input type="submit" value="Clean" name="op">
+</form>
+</div>
+<div class="display">
 <?php
-date_default_timezone_set("Asia/Taipei");  //設定時區
-echo date("Y年m月j日 G時i分s秒");  //第1個參數是時間格式；第2個參數是引數，沒有引數時，默認為現在時間。
-//date("Y-n-j G:i:s",1393224543);  //逗點之後的數值為引數。
+include "encryptdemo5.php";
 ?>
+</div>
+</body>
+</html>
